2540e3f580abcf5054b3f3a7b950eb2fa8d7a557
WuyuetingTina <y9wu@ucsd.edu>
2021-03-04 10:40:32 -0800

    up

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,18 @@
 var labs = require('../labs.json');
 
 exports.view = function(req, res){
+	console.log(labs);
   if(labs.length < 9) {
-  	var todaysLab = [{
+  	var todaysLab = {
 		"id": "lab-9",
 		"title": "Debugging 101",
 		"date": "March 7",
 		"image": "debug.png"
-	}];
+	};
 
-  	labs.push(todaysLab);
+	  labs.push(todaysLab);
+	  console.log(11111111);
+	  console.log(labs);
   }
 
   res.render('index', {
